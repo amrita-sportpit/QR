fa977672b6ab3968fd9c74aa148dd58d416d4df1
amrita-sportpit <amrita.sport.work@gmail.com>
2025-06-27 14:29:38 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/amrita-site/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/QR/' : '/',
 })
